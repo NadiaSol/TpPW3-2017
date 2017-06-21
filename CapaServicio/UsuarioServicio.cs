da614e34ed95044d3ec24b1600e3dd7d1fa1d1dc
PaliFerrara <pao.ferrara@live.com>
2017-06-21 16:10:34 -0300

    Corregido

---
+++ CapaServicio/UsuarioServicio.cs
@@ -0,0 +1,27 @@
+﻿using CapaServicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaServicio
+{
+
+        public class UsuarioServicio
+        {
+        private Entities db = new Entities();
+        public Usuarios Autenticar(Usuarios usuario)
+        {
+
+            if (db.Usuarios.Any(x=>x.Password== usuario.Password) && db.Usuarios.Any(x => x.NombreUsuario == usuario.NombreUsuario))
+            {
+                return new Usuarios { NombreUsuario = "Administrador"};
+            }
+
+            return null;
+        }
+    }
+
+    
+}
