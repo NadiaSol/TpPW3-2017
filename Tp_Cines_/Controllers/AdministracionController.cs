f94c116f9525075d97db544843ac1782e078a9d0
PaliFerrara <pao.ferrara@live.com>
2017-06-26 01:03:39 -0300

    administracion
    nuevo commit

--- Tp_Cines_/Controllers/AdministracionController.cs
+++ Tp_Cines_/Controllers/AdministracionController.cs
@@ -1,21 +1,26 @@
 ﻿using CapaServicio;
+using System.Linq;
 using System.Web.Mvc;
+
 
 namespace Tp_Cines_.Controllers
 {
     public class AdministracionController : Controller
     {
+        //
+        // GET: /Administracion/
 
         Entities ctx = new Entities();
-
         private SedeServicio _sedeServicio = new SedeServicio();
+
         public ActionResult Inicio()
         {
             return View();
         }
         public ActionResult Peliculas()
         {
-            return View();
+            var us = from p in ctx.Peliculas orderby p.Nombre ascending select p;
+            return View(us);
         }
         public ActionResult Sedes()
         {
@@ -24,14 +29,12 @@ namespace Tp_Cines_.Controllers
         [HttpPost]
         public ActionResult Sedes(Sedes sede)
         {
-
-
             if (sede != null)
             {
                 if (ModelState.IsValid)
                 {
                     _sedeServicio.Create(sede);
-                        return View();
+                    return View();
                 }
             }
             ViewBag.Mensaje = "Valores incorrectos";
