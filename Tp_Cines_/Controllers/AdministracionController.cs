7c00a8f85536d78e0c7ec9317522a226e75a80c0
PaliFerrara <pao.ferrara@live.com>
2017-06-26 00:46:39 -0300

    Guarda Sede
    Guarda Sede y valida contra el modelo

--- Tp_Cines_/Controllers/AdministracionController.cs
+++ Tp_Cines_/Controllers/AdministracionController.cs
@@ -1,12 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using CapaServicio;
+using System.Web.Mvc;
 
 namespace Tp_Cines_.Controllers
 {
     public class AdministracionController : Controller
     {
-        //
-        // GET: /Administracion/
 
+        Entities ctx = new Entities();
+
+        private SedeServicio _sedeServicio = new SedeServicio();
         public ActionResult Inicio()
         {
             return View();
@@ -17,6 +19,22 @@ namespace Tp_Cines_.Controllers
         }
         public ActionResult Sedes()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Sedes(Sedes sede)
+        {
+
+
+            if (sede != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    _sedeServicio.Create(sede);
+                        return View();
+                }
+            }
+            ViewBag.Mensaje = "Valores incorrectos";
             return View();
         }
         public ActionResult Carteleras()
