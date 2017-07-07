ca06e75eb3a20eab0b5c9da60826db4dbc351309
PaliFerrara <pao.ferrara@live.com>
2017-07-07 19:01:06 -0300

    Calcular horarios corregido

--- CapaServicio/PeliculaServicio.cs
+++ CapaServicio/PeliculaServicio.cs
@@ -20,19 +20,25 @@ namespace CapaServicio
 
         public List<int> Horarios(int funcion, int idPelicula)
         {
+            var minutosFuncion=funcion * 60; //calculo minutos
             var duracion = GetById(idPelicula).Duracion;
 
             var horarios = new List<int> { funcion };
             var i = 1;
             for (i = 1; i <= 6; i++)
             {
-                funcion = duracion + 30;
-                horarios.Add(funcion);
+                minutosFuncion += duracion + 30;
+                int horaFuncion = minutosFuncion / 60;
+                if(horaFuncion>24)
+                {
+                    horaFuncion = horaFuncion - 24;
+                }
+                horarios.Add(horaFuncion);
             }
 
             return horarios;
         }
 
-
+        
     }
 }
